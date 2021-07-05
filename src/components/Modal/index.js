b33e11d2e79ccb8779c12fc8fdfc041af47b7b23
rodrijl <rodryjl.91@gmail.com>
2021-07-05 03:17:00 -0300

    refactor and adding Proptypes

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,7 @@
 import classnames from 'classnames';
-import { memo, useState} from 'react';
+import { memo } from 'react';
 import Form from '../Form';
-
+import PropTypes from 'prop-types';
 
 
 //components
@@ -10,35 +10,55 @@ import FontAwesomeIcon, { faTimes } from '../FontAwesomeIcon';
 //styles
 import * as styles from './styles.module.scss';
 
-export default memo(function Modal({
+function Modal({
   title,
+  description,
   items,
   actions,
   onCancel
-  }) {
-
-  const handleCancelModal = () => {
-    onCancel()
-  }
+}) {
   //bind styles
   classnames.bind(styles);
-  
+
+  const handleCancelModal = () => {
+    onCancel();
+  };
+
   return (
-      <div className={styles.modalContainer}>
-        <div className={styles.modalContent}>
-          <FontAwesomeIcon
-            className={styles.modalIcon}
-            icon={faTimes}
-            color={'#FFFFFF'}
-            onClick = {handleCancelModal}
-          />
-            {title}
-          <Form
+    <div className={styles.modalContainer}>
+      <div className={styles.modalContent}>
+        <FontAwesomeIcon
+          className={styles.modalIcon}
+          icon={faTimes}
+          color={'#FFFFFF'}
+          onClick={handleCancelModal}
+        />
+        <h2>{title}</h2>
+        <span>{description}</span>
+        <Form
           items={items}
           actions={actions}
           />
-        </div>
       </div>
-    )
-  }
-)
+    </div>
+  );
+}
+
+//#region props definitions
+// default values and types
+Modal.defaultProps = {
+  title:'',
+  description: '',
+  items: []
+};
+
+Modal.propTypes = {
+  onCancel: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  items: PropTypes.array,
+  actions: PropTypes.array.isRequired
+};
+//#endregion
+
+export default memo(Modal); 
