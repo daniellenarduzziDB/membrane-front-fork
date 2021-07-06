9e28bd1dd30bf9e7fbfaa355c9c97999a316fa1f
Campana Damian <damian@aridosoftware.com>
2021-07-06 11:31:06 -0300

    set color

--- src/views/Main/BuySell/index.js
+++ src/views/Main/BuySell/index.js
@@ -6,6 +6,7 @@ import * as styles from './styles.module.scss';
 
 //components
 import ButtonGroup from '../../../components/ButtonGroup';
+import Switch from '../../../components/Switch';
 
 export default memo(function BuySell() {
   //bind styles
@@ -22,6 +23,8 @@ export default memo(function BuySell() {
         stylingMode="contained"
         onItemClick={handleItemClick}
       />
+
+      <Switch isChecked={true} onChange={e => console.log(e)} />
     </div>
   );
 });
